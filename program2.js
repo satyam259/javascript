50998598ccec6b11ba096af023a6b6ea77a7fc8f
satyam <satyamwatts@gmail.com>
2024-07-07 16:16:05 +0530

    calculate power

---
+++ program2.js
@@ -0,0 +1,7 @@
+// Create a function that takes voltage and current and returns the calculated power.
+
+function circuitPower(voltage, current) {
+  return voltage * current;
+}
+const power = circuitPower(400, 10);
+console.log(power);
