7df29b058b2ac7a64de2069bc3d52e8f87d5ea07
satyam <satyamwatts@gmail.com>
2024-07-07 16:29:23 +0530

    find range of triangle third's edge

---
+++ program3.js
@@ -0,0 +1,7 @@
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+function nextEdge(side1, side2) {
+  return side1 + side2 - 1;
+}
+const thirdedge = nextEdge(3, 4);
+console.log(thirdedge);
