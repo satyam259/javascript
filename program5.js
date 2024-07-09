ef53d9dce4ed4e924ca71f087274ad842224e37f
satyam <satyamwatts@gmail.com>
2024-07-09 22:48:27 +0530

    get minimum and maximum number in array

---
+++ program5.js
@@ -0,0 +1,21 @@
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+function minMax(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let minmax = [];
+  for (let i = 0; i <= arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  for (let i = 0; i <= arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  minmax.push(min, max);
+  return minmax;
+}
+const a = minMax([1, 4, 0, 3, 9]);
+console.log(a);
