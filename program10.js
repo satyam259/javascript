4db7984db819d73ec5336c3c2dbf3a56643f118b
satyam <satyamwatts@gmail.com>
2024-07-21 22:16:30 +0530

    sort an array on basis of price

---
+++ program10.js
@@ -0,0 +1,17 @@
+//sort an array of objects on basis of price
+
+function sortDrinkByPrice(drinks) {
+  // const a= drinks.sort((p1, p2) =>
+  // (p1.price > p2.price) ? 1
+  // : (p1.price < p2.price) ? -1 :
+  // 0);
+  const a = drinks.sort((p1, p2) => p1.price - p2.price);
+  return a;
+}
+const b = [
+  { name: "lemonade", price: 50 },
+  { name: "lime", price: 10 },
+  { name: "banana", price: 20 },
+];
+const c = sortDrinkByPrice(b);
+console.log(c);
